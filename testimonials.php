9e159676c22bbae9596d0403b889a35ad443f8ad
Siddharth9792 <siddharthamaurya800@gmail.com>
2025-06-10 11:39:05 +0530

    first commit

--- testimonials.php
+++ testimonials.php
@@ -187,7 +187,7 @@
                     <div class="page_breadcrumb">
                         <nav aria-label="breadcrumb">
                             <ol class="breadcrumb">
-                                <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                                <li class="breadcrumb-item"><a href="index.html">Home</a></li>
                                 <li class="breadcrumb-item active" aria-current="page">Testimonials</li>
                             </ol>
                         </nav>
